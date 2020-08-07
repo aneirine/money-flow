8d300ceef9a3650ff6a67831628bf64fb260393e
Aneirine <anyashi2002@gmail.com>
2020-08-08 00:37:30 +0300

    Add transaction data fields

--- src/main/java/com/aneirine/moneyflow/api/transactions/main/domain/TransactionData.java
+++ src/main/java/com/aneirine/moneyflow/api/transactions/main/domain/TransactionData.java
@@ -1,4 +1,13 @@
 package com.aneirine.moneyflow.api.transactions.main.domain;
 
+import lombok.Data;
+
+@Data
 public class TransactionData {
+
+    private String name;
+    private double sum;
+    private int typeOrdinal;
+    private long categoryId;
+
 }
