417a11a4ad6a6c88194fef7844ddbce6a65c230a
Aneirine <anyashi2002@gmail.com>
2020-08-08 00:16:32 +0300

    Add transaction category

---
+++ src/main/java/com/aneirine/moneyflow/entities/TransactionCategory.java
@@ -0,0 +1,23 @@
+package com.aneirine.moneyflow.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "transaction_categories")
+public class TransactionCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+}
