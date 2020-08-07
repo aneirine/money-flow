f84412d93e367d8214bc8b71caec164b55230616
Aneirine <anyashi2002@gmail.com>
2020-08-08 00:15:49 +0300

    Add transaction entity

---
+++ src/main/java/com/aneirine/moneyflow/entities/Transaction.java
@@ -0,0 +1,25 @@
+package com.aneirine.moneyflow.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private double sum;
+
+}
