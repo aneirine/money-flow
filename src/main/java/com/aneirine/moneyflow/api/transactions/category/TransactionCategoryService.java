bb430a282889381a2c3de3bab37311db73cd8714
Aneirine <anyashi2002@gmail.com>
2020-08-08 00:33:28 +0300

    Add all crud methods in servie

--- src/main/java/com/aneirine/moneyflow/api/transactions/category/TransactionCategoryService.java
+++ src/main/java/com/aneirine/moneyflow/api/transactions/category/TransactionCategoryService.java
@@ -4,6 +4,7 @@ import com.aneirine.moneyflow.api.transactions.category.domain.TransactionCatego
 import com.aneirine.moneyflow.api.transactions.category.domain.TransactionCategoryResponse;
 import com.aneirine.moneyflow.entities.TransactionCategory;
 import com.aneirine.moneyflow.exceptions.ConflictException;
+import com.aneirine.moneyflow.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +15,35 @@ public class TransactionCategoryService {
     @Autowired
     private TransactionCategoryRepository transactionCategoryRepository;
 
-    public TransactionCategoryResponse createTransactionCategory(TransactionCategoryData data){
-        TransactionCategory transactionCategory = new TransactionCategory();
-        if(transactionCategoryRepository.existsByName(data.getName())){
+    public TransactionCategoryResponse createTransactionCategory(TransactionCategoryData data) {
+        TransactionCategory category = new TransactionCategory();
+        if (transactionCategoryRepository.existsByName(data.getName())) {
             throw new ConflictException("NAME_ALREADY_EXISTS");
         }
-        transactionCategory.setName(data.getName());
-        transactionCategoryRepository.save(transactionCategory);
-        return new TransactionCategoryResponse(transactionCategory);
+        category.setName(data.getName());
+        transactionCategoryRepository.save(category);
+        return new TransactionCategoryResponse(category);
+    }
 
+    public TransactionCategoryResponse getTransactionCategoryById(long id) {
+        return new TransactionCategoryResponse(transactionCategoryRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("TRANSACTION_CATEGORY_NOT_FOUND")));
+    }
+
+    public TransactionCategoryResponse updateTransactionCategoryById(long id, TransactionCategoryData data) {
+        TransactionCategory category = transactionCategoryRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("TRANSACTION_CATEGORY_NOT_FOUND"));
+        if (transactionCategoryRepository.existsByName(data.getName())) {
+            throw new ConflictException("NAME_ALREADY_EXISTS");
+        }
+        category.setName(data.getName());
+        transactionCategoryRepository.save(category);
+        return new TransactionCategoryResponse(category);
+    }
+
+    public void deleteTransactionCategoryById(long id) {
+        transactionCategoryRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("TRANSACTION_CATEGORY_NOT_FOUND"));
+        transactionCategoryRepository.deleteById(id);
     }
 }
