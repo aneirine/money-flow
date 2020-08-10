b11d3fb8c10e1db371f5d271d137fc6c08918565
Aneirine <anyashi2002@gmail.com>
2020-08-10 19:39:21 +0300

    Add transaction service

---
+++ transaction-service/src/main/java/com/aneirine/transactionservice/MoneyFlowApplication.java
@@ -0,0 +1,13 @@
+package com.aneirine.transactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyFlowApplication.class, args);
+	}
+
+}
---
+++ transaction-service/src/test/java/com/aneirine/transactionservice/MoneyFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aneirine.transactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
