5dc72c7f765a47be692fa77caaf5a390d5177766
Aneirine <anyashi2002@gmail.com>
2020-08-10 19:26:41 +0300

    Set properties for User-service

--- user-service/src/main/java/com/aneirine/moneyflow/MoneyFlowApplication.java
+++ user-service/src/main/java/com/aneirine/moneyflow/MoneyFlowApplication.java
@@ -2,12 +2,15 @@ package com.aneirine.moneyflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MoneyFlowApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoneyFlowApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyFlowApplication.class, args);
+    }
 
 }
