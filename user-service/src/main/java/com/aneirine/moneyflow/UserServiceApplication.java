e9d5fe78404d20c2611c2e5623502c4811a21c51
Aneirine <anyashi2002@gmail.com>
2020-08-11 15:41:29 +0300

    Add feign to services

--- user-service/src/main/java/com/aneirine/moneyflow/UserServiceApplication.java
+++ user-service/src/main/java/com/aneirine/moneyflow/UserServiceApplication.java
@@ -3,10 +3,12 @@ package com.aneirine.moneyflow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class UserServiceApplication {
 
     public static void main(String[] args) {
