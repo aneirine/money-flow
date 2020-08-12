cd3cd0552cff56af2585cd1edb4f3b597694496e
Aneirine <anyashi2002@gmail.com>
2020-08-12 16:07:28 +0300

    Add config

--- user-service/src/main/java/com/aneirine/moneyflow/TransactionFeignService.java
+++ user-service/src/main/java/com/aneirine/moneyflow/TransactionFeignService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "transactions-service")
+@FeignClient(name = "transaction-service")
 @RequestMapping("/api/transactions")
 public interface TransactionFeignService {
 
