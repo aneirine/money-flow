aca806d71be490477ce79d943ae399b00544078c
Aneirine <anyashi2002@gmail.com>
2020-08-12 15:45:15 +0300

    Add all methods for users

--- user-service/src/main/java/com/aneirine/moneyflow/api/users/UserRepository.java
+++ user-service/src/main/java/com/aneirine/moneyflow/api/users/UserRepository.java
@@ -4,4 +4,8 @@ import com.aneirine.moneyflow.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
+    User findByEmail(String email);
 }
--- user-service/src/main/java/com/aneirine/moneyflow/api/users/domain/UserResponse.java
+++ user-service/src/main/java/com/aneirine/moneyflow/api/users/domain/UserResponse.java
@@ -1,5 +1,6 @@
 package com.aneirine.moneyflow.api.users.domain;
 
+import com.aneirine.moneyflow.entities.User;
 import lombok.Data;
 
 @Data
@@ -8,5 +9,11 @@ public class UserResponse {
     private long id;
     private String username;
     private String email;
+
+    public UserResponse(User user){
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+    }
 
 }
