94a21ba26755d40f573d51fd4f2f03185f071568
Aneirine <anyashi2002@gmail.com>
2020-08-13 13:02:23 +0300

    Add dependency to transaction service

--- user-service/src/main/java/com/aneirine/moneyflow/api/feign/TransactionFeignService.java
+++ user-service/src/main/java/com/aneirine/moneyflow/api/feign/TransactionFeignService.java
@@ -1,18 +1,12 @@
 package com.aneirine.moneyflow.api.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name = "transaction-service")
 @RequestMapping("/api/transactions")
 public interface TransactionFeignService {
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/test/{userId}")
-    public ResponseEntity creteTestTransactionWithUserId(@PathVariable("userId") long userId);
 
 
 }
