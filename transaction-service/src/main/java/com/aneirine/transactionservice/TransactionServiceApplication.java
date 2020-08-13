0b05538e0a26beaca48ac6d63fcba01e037097c1
Aneirine <anyashi2002@gmail.com>
2020-08-13 12:43:10 +0300

    Fixes

--- transaction-service/src/main/java/com/aneirine/transactionservice/TransactionServiceApplication.java
+++ transaction-service/src/main/java/com/aneirine/transactionservice/TransactionServiceApplication.java
@@ -3,14 +3,13 @@ package com.aneirine.transactionservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class TransactionServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TransactionServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionServiceApplication.class, args);
+    }
 
 }
