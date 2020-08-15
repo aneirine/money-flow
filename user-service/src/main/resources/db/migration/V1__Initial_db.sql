f617ac871743411d69dcba7c2c75ee83dd3d8d8f
Aneirine <anyashi2002@gmail.com>
2020-08-15 17:58:39 +0300

    Add V1 sql for user service

---
+++ user-service/src/main/resources/db/migration/V1__Initial_db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users (
+id BIGSERIAL NOT NULL PRIMARY KEY,
+username VARCHAR (256) NOT NULL,
+username VARCHAR (256) NOT NULL UNIQUE,
+)
+
+CREATE TABLE IF NOT EXISTS user_transaction_id_list (
+     user_id INTEGER NOT NULL REFERENCES users(user_id),
+    transaction_id_list  INTEGER NOT NULL 
+)
