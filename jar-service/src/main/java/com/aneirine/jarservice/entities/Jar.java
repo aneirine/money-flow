46b8afea93cb69024a3178efacc758cd78da536d
Aneirine <anyashi2002@gmail.com>
2020-08-22 16:23:01 +0300

    Add fields to entity

--- jar-service/src/main/java/com/aneirine/jarservice/entities/Jar.java
+++ jar-service/src/main/java/com/aneirine/jarservice/entities/Jar.java
@@ -1,9 +1,19 @@
 package com.aneirine.jarservice.entities;
 
 
+import com.aneirine.jarservice.entities.enums.JarStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Date;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "jars")
 public class Jar {
@@ -15,5 +25,10 @@ public class Jar {
     private String aim;
     private Date startDate;
     private Date endDate;
+    private JarStatus status;
+    private String description;
+    private double sumGoal;
+    private double sumCurrent;
+
 
 }
