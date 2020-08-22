a273cdcb5255005285853852057e480eb413d4d7
Aneirine <anyashi2002@gmail.com>
2020-08-22 16:28:04 +0300

    Create data

--- jar-service/src/main/java/com/aneirine/jarservice/api/jars/domain/JarData.java
+++ jar-service/src/main/java/com/aneirine/jarservice/api/jars/domain/JarData.java
@@ -1,4 +1,16 @@
 package com.aneirine.jarservice.api.jars.domain;
 
+import lombok.Getter;
+
+@Getter
 public class JarData {
+
+    private String aim;
+    private String startDate;
+    private String endDate;
+    private int statusJar;
+    private String description;
+    private double sumGoal;
+
+
 }
