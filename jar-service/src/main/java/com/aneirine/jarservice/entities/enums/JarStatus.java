1dac39b314066c88fc81a0418083638c78cbc4cf
Aneirine <anyashi2002@gmail.com>
2020-08-22 22:40:44 +0300

    Add find by ordinal method in jar status

--- jar-service/src/main/java/com/aneirine/jarservice/entities/enums/JarStatus.java
+++ jar-service/src/main/java/com/aneirine/jarservice/entities/enums/JarStatus.java
@@ -1,16 +1,18 @@
 package com.aneirine.jarservice.entities.enums;
 
+import com.aneirine.jarservice.exceptions.NotFoundException;
+
 public enum JarStatus {
 
     ACTIVE, CLOSED;
 
 
-    public static JarStatus getJarStatusByOrdinal(int ordinal){
-        for(JarStatus temp : JarStatus.values()){
-            if(temp.ordinal() == ordinal){
+    public static JarStatus getJarStatusByOrdinal(int ordinal) {
+        for (JarStatus temp : JarStatus.values()) {
+            if (temp.ordinal() == ordinal) {
                 return temp;
             }
         }
-        throw
+        throw new NotFoundException("JAR_STATUS_NOT_FOUND");
     }
 }
