92eed56bf61481c7ae4864c20fdc3dd7653d82e1
Aneirine <anyashi2002@gmail.com>
2020-09-01 13:57:04 +0300

    Add method to feign service

--- jar-service/src/main/java/com/aneirine/moneyflow/api/feign/UserFeignService.java
+++ jar-service/src/main/java/com/aneirine/moneyflow/api/feign/UserFeignService.java
@@ -1,12 +1,19 @@
 package com.aneirine.moneyflow.api.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name = "user-service")
 @RequestMapping("/api/users")
 public interface UserFeignService {
 
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/{userId}/jars/{jarId}")
+    public ResponseEntity addJarToUser(@PathVariable("userId") long userId,
+                                       @PathVariable("jarId") long jarId);
 
 
 }
