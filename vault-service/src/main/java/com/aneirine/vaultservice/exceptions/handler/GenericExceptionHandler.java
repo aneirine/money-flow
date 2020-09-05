a09de3bbcb417526e7c6f280d7a31a958535df99
Aneirine <anyashi2002@gmail.com>
2020-09-05 13:22:05 +0300

    Last fixes

--- vault-service/src/main/java/com/aneirine/vaultservice/exceptions/handler/GenericExceptionHandler.java
+++ vault-service/src/main/java/com/aneirine/vaultservice/exceptions/handler/GenericExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.aneirine.vaultservice.exceptions.handler;
 
 
+import com.aneirine.vaultservice.exceptions.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
